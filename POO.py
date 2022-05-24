5c6ebf20bbab063d57cfef1185f956ee4b56f253
LUIS <daemo159@gmail.com>
2022-05-24 12:49:57 -0500

    modified poo.py

--- POO.py
+++ POO.py
@@ -1,6 +1,14 @@
+
 class persona():
-    def  __init__(self,nombre,edad):
+
+    def  __init__(self,nombre,edad,cargo):
         self.nombre = nombre
         self.edad = edad
+        self.cargo = cargo
+    def info(self):
+        infor = ("Hola! soy {}, tengo {} y soy {}")
+        print(infor.format(self.nombre,self.edad,self.cargo))
 
-persona1 = persona("beta",21)
+persona1 = persona("ricardo",64,"Magister en desocupe")
+
+persona1.info()
